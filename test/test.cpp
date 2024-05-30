c31b4a6c42c4b783aa35c90efd6463ecd983cf5e
McCallisterRomer <47375742+McCallisterRomer@users.noreply.github.com>
2024-05-29 21:13:43 -0500

    Add Action (#2)
    * add action
    
    * rename test workflow
    
    * dont run build on pr
    
    * try different action name
    
    * action name wants path
    
    * fix tag version
    
    * debug solution for artifact name
    
    * try env
    
    * typos
    
    * print more env vars
    
    * generate driver manifest
    
    * add manifest to github env
    
    * write manifest to install path
    
    * add test
    
    * dont change dir
    
    * try normalizing path separators
    
    * add more os types
    
    * c++17
    
    * check win2019
    
    * add version check to test; target new artifacts
    
    * add back older ubuntu; fix artifact name
    
    * temp disable version check
    
    * add back version check
    
    * try GITHUB_OUTPUT
    
    * missing outputs property
    
    * cleanup
    
    * try macOS
    
    * space
    
    * add all macOS versions
    
    * for x64 for macOS
    
    * rename test workflow
    
    * outputs
    
    * set outputs
    
    * missing outputs property
    
    * update readme
    
    * macOS architectures
    
    * need quotes
    
    * add environment var test
    
    * update readme
    
    * remove macOS architectures
    
    * print workflow inputs
    
    * single input
    
    * again
    
    * parameterize on tag
    
    * fix

---
+++ test/test.cpp
@@ -0,0 +1,98 @@
+#include <vulkan/vulkan.h>
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef VKCI_API_VERSION
+#error Must build test with VKCI_API_VERSION defined
+#endif
+
+constexpr auto requestedVersionString = std::string_view{VKCI_API_VERSION};
+constexpr auto validationLayers = std::string_view{"VK_LAYER_KHRONOS_validation"};
+constexpr auto swiftshaderDeviceName = std::string_view{"SwiftShader Device (LLVM 10.0.0)"};
+
+int main()
+{
+    auto version = uint32_t{};
+    vkEnumerateInstanceVersion(&version);
+    const auto actualVersionString = std::to_string(VK_API_VERSION_MAJOR(version)) + "."
+                                   + std::to_string(VK_API_VERSION_MINOR(version)) + "."
+                                   + std::to_string(VK_API_VERSION_PATCH(version));
+
+    const auto expectedVersionString = []()
+    {
+        if (std::count(requestedVersionString.begin(), requestedVersionString.end(), '.') == 3)
+        {
+            return requestedVersionString.substr(0, requestedVersionString.rfind('.'));
+        }
+
+        return requestedVersionString;
+    }();
+
+    if (actualVersionString != expectedVersionString)
+    {
+        std::cerr << "Failed: Unexpected Vulkan version. expected: " << expectedVersionString << ", actual: " << actualVersionString << '\n';
+        return 1;
+    }
+
+    auto layerCount = uint32_t{};
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    auto availableLayers = std::vector<VkLayerProperties>(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    const auto layerPos = std::find_if(availableLayers.cbegin(), availableLayers.cend(), [](const auto layer)
+    {
+        return validationLayers == layer.layerName;
+    });
+
+    if (layerPos == availableLayers.cend())
+    {
+        std::cerr << "Failed: did not find validation layers\n";
+        return 1;
+    }
+
+    auto appInfo = VkApplicationInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.apiVersion = version;
+    auto createInfo = VkInstanceCreateInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    const auto layers = std::array{ validationLayers.data() };
+    createInfo.enabledLayerCount = static_cast<uint32_t>(layers.size());
+    createInfo.ppEnabledLayerNames = layers.data();
+    auto instance = VkInstance{};
+    if (auto result = vkCreateInstance(&createInfo, nullptr, &instance); result)
+    {
+        std::cerr << "Failed: vkCreateInstance failed with: " << result << '\n';
+        return 1;
+    }
+
+    auto deviceCount = uint32_t{};
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0)
+    {
+        std::cerr << "Failed: did not find any physical devices\n";
+        return 1;
+    }
+
+    auto devices = std::vector<VkPhysicalDevice>(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    const auto swiftshaderPos = std::find_if(devices.cbegin(), devices.cend(), [](auto device)
+    {
+        auto properties = VkPhysicalDeviceProperties{};
+        vkGetPhysicalDeviceProperties(device, &properties);
+        return swiftshaderDeviceName == properties.deviceName;
+    });
+
+    if (swiftshaderPos == devices.cend())
+    {
+        std::cerr << "Failed: did not find SwiftShader\n";
+        return 1;
+    }
+
+    std::cout << "Passed\n";
+    return 0;
+}
